e6557198047617b50273e10aa70c0f046a67a6a6
jschiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2022-11-06 12:39:30 -0500

    pure lookups of resource locations

--- README.md
+++ README.md
@@ -4,20 +4,27 @@ Experiment for an onchain strategy explorer simulation in solidity, inspired by 
 
 The goal of this is repo is really just to explain a concept of generative landscapes with RPG qualities that use onchain mechanisms.
 
-Named after the region in the game Heroes2
-
 <img width="645" alt="interpreter" src="https://user-images.githubusercontent.com/9449596/200182552-cf5387a6-20a6-4d1f-875b-3d1a62dd4c53.png">
+(Image from Heroes2)
 
 ## How it works
 
-The world is an arbitrary square grid. There are different types of resources that randomly generate and can appear as you explore the grid.
+The world is an arbitrary square grid. There are different types of resources that randomly generate and can appear as you explore the grid. Storage is expensive, but read-only logic is cheap. No map is stored onchain, but rather the generative landscape is explored using an (X,Y) plain that reveals different types of resources.
 
 The constuctor for the main contract library takes a seed hash. Change this to generate a different continent every time.
 
 Icon pack by Shikashi:
 https://cheekyinkling.itch.io/shikashis-fantasy-icons-pack
 
-| Item         | IPFS     |
-| ------------ | -------- |
-| Juicy Apples | 1.99     |
-| Bananas      | **1.89** |
+| Item         | IPFS                                           | Index |
+| ------------ | ---------------------------------------------- | ----- |
+| Gold         | QmRkhkX4uFycLzdinFzS1jLL6nsKQt3uqSXWJ6EDuvPeDT | 1     |
+| Apples       | QmcB2zV44HVcnEjfdJMsoM1dbN89SYE4QSY4tpewmxK9Y9 | 2     |
+| Wood         | QmeLAVCEXBjaBKdKqKYZvuxxyQ2FmrChRCbBZAPkXdGXWB | 3     |
+| Empty Square | QmZGqm9UrsQzTmus8HSipN5v1kaTXK6avngGMshevZC3Pu | 0     |
+
+## Trivial Additions
+
+- Make the resources "claimable" as ERC1155s
+- Create a player-character that is always at a certain x,y position and move-able
+- allow claiming the resources only when the player is within 1 x,y of any particular location
--- contracts/EnrothExplorer.sol
+++ contracts/EnrothExplorer.sol
@@ -1,9 +1,56 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
 contract EnrothExplorer {
-    
+    uint256 public seed;
+
+    struct Resource{
+        string name;
+        string URI;
+    }
+
+    mapping (uint => Resource) public resources;
+
+    constructor(uint256 _seed){
+      seed = _seed;
+
+      //set resources
+      Resource memory nothing = Resource("Fields (Nothing)", "ipfs://QmZGqm9UrsQzTmus8HSipN5v1kaTXK6avngGMshevZC3Pu");
+      resources[0] = nothing;
+
+      Resource memory gold = Resource("Gold", "ipfs://QmRkhkX4uFycLzdinFzS1jLL6nsKQt3uqSXWJ6EDuvPeDT");
+      resources[1] = gold;
+
+      Resource memory wood = Resource("Wood", "ipfs://QmeLAVCEXBjaBKdKqKYZvuxxyQ2FmrChRCbBZAPkXdGXWB");
+      resources[2] = wood;
+
+      Resource memory apple = Resource("Apples", "ipfs://QmcB2zV44HVcnEjfdJMsoM1dbN89SYE4QSY4tpewmxK9Y9");
+      resources[3] = apple;
+
+    }
+
+    //returns correlate to resources mapping for what kind of item you can find here, if any
+    function getPlotItem(bytes32 hash) external pure returns(uint){
+      //its possible that Gold AND another resource could 'spawn' in the same square. Gold takes priority.
+      if(uint(hash) % 9999 == 0){
+        //1 in 9999 change of finding gold
+        return 1;
+      }else if(uint(hash) % 500 == 0){
+        return 2;
+      }else if((uint(hash) + 1 )% 500 == 0){
+        return 3;
+        //apples and wood both have a 1/500 chance of being found, but never on the same square
+      }else{
+        return 0;
+      }
+    }
+
+    function exploreMap(uint256 x, uint256 y) external view returns(bytes32) {
+      //without a seed, the world will look very static and be the same every time this is used.
+      //with the seed, the hash returned is different for each instance of Enroth
+      return keccak256(abi.encodePacked(x, y, seed));
+    }
 }
